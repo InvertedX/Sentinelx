816bda1f22af6d66a056695e5f29eb09a53f8ab0
Sarath <sarathkumars677@gmail.com>
2019-08-20 23:04:53 +0530

    CryptoChannel for address generation and validation

--- android/app/src/main/kotlin/com/invertedx/sentinelx/channel/CryptoChannel.kt
+++ android/app/src/main/kotlin/com/invertedx/sentinelx/channel/CryptoChannel.kt
@@ -1,6 +1,11 @@
 package com.invertedx.sentinelx.channel
 
 import android.content.Context
+import com.invertedx.sentinelx.SentinelxApp
+import com.invertedx.sentinelx.hd.HD_Account
+import com.invertedx.sentinelx.i
+import com.invertedx.sentinelx.segwit.P2SH_P2WPKH
+import com.invertedx.sentinelx.segwit.SegwitAddress
 import com.invertedx.sentinelx.utils.FormatsUtil
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -17,7 +22,21 @@ class CryptoChannel(val applicationContext: Context) : MethodChannel.MethodCallH
         }
 
         when (methodcall.method) {
-
+            "generateAddressBIP49" -> {
+                val accountIndex = methodcall.argument<Int>("account_index")
+                val xpub = methodcall.argument<String>("xpub")
+                result.success(this.generateAddressBIP49(xpub, accountIndex))
+            }
+            "generateAddressBIP84" -> {
+                val accountIndex = methodcall.argument<Int>("account_index")
+                val xpub = methodcall.argument<String>("xpub")
+                result.success(this.generateAddressBIP84(xpub, accountIndex))
+            }
+            "generateAddressXpub" -> {
+                val accountIndex = methodcall.argument<Int>("account_index")
+                val xpub = methodcall.argument<String>("xpub")
+                result.success(this.generateAddressXpub(xpub, accountIndex))
+            }
             "validateXPUB" -> {
                 this.validateXPUB(methodcall.argument<String>("xpub"), result)
             }
@@ -28,6 +47,66 @@ class CryptoChannel(val applicationContext: Context) : MethodChannel.MethodCallH
         }
 
     }
+
+    fun generateAddressBIP49(xpub: String?, account_index: Int?): String {
+        if (xpub == null || account_index == null) {
+            return ""
+
+        }
+        val wallet = HD_Account(SentinelxApp.networkParameters, xpub, "", 0)
+        wallet.getChain(0).addrIdx = account_index
+        val address = wallet.getChain(0).getAddressAt(account_index)
+        val ecKey = address.ecKey
+        val p2sh_p2wpkh = P2SH_P2WPKH(ecKey.pubKey, SentinelxApp.networkParameters)
+        val addr = p2sh_p2wpkh.addressAsString
+        i("generateAddressBIP49 $addr")
+        return addr
+    }
+
+    fun generateAddressBIP84(xpub: String?, account_index: Int?): String {
+
+        if (xpub == null || account_index == null) {
+            return ""
+
+        }
+        val wallet = HD_Account(SentinelxApp.networkParameters, xpub, "", 0)
+        wallet.getChain(0).addrIdx = account_index
+
+        val hdAddress = wallet.getChain(0).getAddressAt(account_index)
+        val ecKey = hdAddress.ecKey
+
+        val segwitAddress = SegwitAddress(ecKey.pubKey, SentinelxApp.networkParameters)
+        val address = segwitAddress.bech32AsString
+        i("generateAddressBIP84 $address")
+        return address
+    }
+
+    fun generateAddressXpub(xpub: String?, account_index: Int?): String {
+
+        if (xpub == null || account_index == null) {
+            return ""
+
+        }
+        val account = HD_Account(SentinelxApp.networkParameters, xpub, "", 0)
+        account.getChain(0).addrIdx = account_index
+        val hdAddress = account.getChain(0).getAddressAt(account_index)
+        i("generateAddressXpub ${hdAddress.addressString}")
+        return hdAddress.addressString
+    }
+//  public fun generateAddress(xpub: String?, change_index: Int?, account_index: Int?): String {
+//
+//        if (xpub == null || change_index == null || account_index == null) {
+//            return ""
+//
+//        }
+//        var wallet = HD_Account(SentinelxApp.networkParameters, xpub, "", 0)
+//        wallet.getChain(0).addrIdx = change_index;
+//        val Add = wallet.getChain(0).getAddressAt(change_index);
+//        val ecKey = Add.ecKey;
+//        val p2sh_p2wpkh = P2SH_P2WPKH(ecKey.pubKey, SentinelxApp.networkParameters)
+//        val addr = p2sh_p2wpkh.addressAsString
+//        return addr;
+//    }
 
     private fun validateAddress(address: String?, result: MethodChannel.Result) {
         if (FormatsUtil.getInstance().isValidBitcoinAddress(address)) {
