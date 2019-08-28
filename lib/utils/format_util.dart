1a89796e95cea3c050549a9b38a30a39a943351d
Sarath <sarathkumars677@gmail.com>
2019-08-28 21:30:41 +0530

    added formatting utilities

--- lib/utils/format_util.dart
+++ lib/utils/format_util.dart
@@ -1,4 +1,17 @@
+import 'package:intl/intl.dart';
 
-String satToBtc(num coin){
-  return (coin/100000000).toDouble().toStringAsFixed(8);
+String satToBtc(num coin) {
+  return (coin / 100000000).toDouble().toStringAsFixed(8);
+}
+
+String formatTime(num timestamp) {
+  var formatter = new DateFormat("h:mm");
+  var time = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+  return formatter.format(time);
+}
+
+String formatDate(num timestamp) {
+  var formatter = new DateFormat("d MMM yyyy");
+  var time = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+  return formatter.format(time);
 }
