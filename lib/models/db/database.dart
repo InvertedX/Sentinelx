f95b23b65d661fa13135096caa0563126301f3ba
Sarath <sarathkumars677@gmail.com>
2019-11-02 15:38:42 +0530

    [UPDATE] database init with password

--- lib/models/db/database.dart
+++ lib/models/db/database.dart
@@ -3,27 +3,27 @@ import 'package:sentinelx/models/db/txDB.dart';
 import 'package:sentinelx/models/wallet.dart';
 import 'package:sentinelx/shared_state/appState.dart';
 
-initDatabase() async {
-  await SentinelxDB.instance.database;
+initDatabase(String pass) async {
+  // ignore: unnecessary_statements
+  await SentinelxDB.instance.init(pass);
   final wallets = await Wallet.getAllWallets();
   AppState appState = AppState();
 
   if (wallets.length == 0) {
     //Default account
     //for future updates app will support multiple account
+    //A single Account can track multiple xpubs and addresses
     var wallet = new Wallet(walletName: "Wallet 1", xpubs: []);
     Wallet.insert(wallet);
     print("Init : Wallet created");
     appState.wallets = wallets.toList();
     appState.wallets.add(wallet);
     appState.selectWallet(wallet);
-    await TxDB.instance(wallet.getTxDb()).database;
-    appState.selectedWallet.initTxDb();
+    appState.selectedWallet.initTxDb(appState.selectedWallet.id);
   } else {
     appState.wallets = wallets.toList();
     appState.selectWallet(wallets.toList().first);
     print(appState.selectedWallet.toJson());
-    await TxDB.instance(wallets.toList().first.getTxDb()).database;
-    appState.selectedWallet.initTxDb();
+    appState.selectedWallet.initTxDb(appState.selectedWallet.id);
   }
 }
