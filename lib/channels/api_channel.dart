894fa4806995ee78a31e08a781f3cf4971863a7f
Sarath <sarathkumars677@gmail.com>
2020-04-25 14:03:15 +0530

    Improved json parsing

--- lib/channels/api_channel.dart
+++ lib/channels/api_channel.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:sentinelx/models/dojo.dart';
 import 'package:sentinelx/models/exchange_rate.dart';
@@ -18,8 +19,7 @@ class ApiChannel {
 
   Future<String> getXpubOrAddress(String xpubOrAddress) async {
     try {
-      String response = await platform
-          .invokeMethod("getTxData", {'xpubOrAddress': xpubOrAddress});
+      String response = await platform.invokeMethod("getTxData", {'xpubOrAddress': xpubOrAddress});
       return response;
     } catch (error) {
       throw error;
@@ -28,8 +28,7 @@ class ApiChannel {
 
   Future<bool> addHDAccount(String xpub, String bip) async {
     try {
-      bool okay = await platform
-          .invokeMethod("addHDAccount", {'xpub': xpub, "bip": bip});
+      bool okay = await platform.invokeMethod("addHDAccount", {'xpub': xpub, "bip": bip});
       return okay;
     } catch (error) {
       throw error;
@@ -39,8 +38,8 @@ class ApiChannel {
   Future<TxDetailsResponse> getTx(String txid) async {
     try {
       String response = await platform.invokeMethod("getTx", {'txid': txid});
-      TxDetailsResponse txDetailsResponse =
-          TxDetailsResponse.fromJson(jsonDecode(response));
+      Map<String, dynamic> map = await parseJSON(response);
+      TxDetailsResponse txDetailsResponse = TxDetailsResponse.fromJson(map);
       return txDetailsResponse;
     } catch (error) {
       print(error);
@@ -50,48 +49,47 @@ class ApiChannel {
 
   Future<String> getUnspent(List<String> xpubsAndAddresses) async {
     String joined = xpubsAndAddresses.join("|");
-    String response =
-        await platform.invokeMethod("unspent", {'params': joined});
+    String response = await platform.invokeMethod("unspent", {'params': joined});
     return response;
   }
 
   Future<DojoAuth> authenticateDojo(String url, String apiKey) async {
-    String status = await platform
-        .invokeMethod("authenticateDojo", {"url": url, "apiKey": apiKey});
-    DojoAuth auth = DojoAuth.fromJson(jsonDecode(status));
+    String status = await platform.invokeMethod("authenticateDojo", {"url": url, "apiKey": apiKey});
+    Map<String, dynamic> map = await parseJSON(status);
+    DojoAuth auth = DojoAuth.fromJson(map);
     return Future.value(auth);
   }
 
-  Future<bool> setDojo(
-      String accessToken, String refreshToken, String url) async {
-    await platform.invokeMethod("setDojo", {
-      'accessToken': accessToken,
-      "refreshToken": refreshToken,
-      "dojoUrl": url
-    });
+  Future<bool> setDojo(String accessToken, String refreshToken, String url) async {
+    await platform.invokeMethod("setDojo", {'accessToken': accessToken, "refreshToken": refreshToken, "dojoUrl": url});
     return Future.value(true);
   }
 
   Future<String> getExchangeRates(String url) async {
-    String response =
-        await platform.invokeMethod("getExchangeRates", {"url": url});
+    String response = await platform.invokeMethod("getExchangeRates", {"url": url});
     return Future.value(response);
   }
 
   Future<String> getNetworkLog() async {
-    String response =
-        await platform.invokeMethod("getNetworkLog");
+    String response = await platform.invokeMethod("getNetworkLog");
     return Future.value(response);
   }
 
-  pushTx(String text) async{
+  pushTx(String text) async {
     try {
-      String address =
-          await platform.invokeMethod<String>("pushTx", {'hex': text});
+      String address = await platform.invokeMethod<String>("pushTx", {'hex': text});
       return address;
     } catch (exception) {
       print(exception);
       throw exception;
     }
+  }
+
+  Future<Map<String, dynamic>> parseJSON(String data) async {
+    return compute(decodeJSON, data);
+  }
+
+  static Map<String, dynamic> decodeJSON(String string) {
+    return jsonDecode(string);
   }
 }
